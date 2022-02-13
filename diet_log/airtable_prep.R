edad04489c7ec311fd99a182d19505f01aa342a4
Kurt R. Peters <kurt.r.peters@gmail.com>
2022-02-12 22:38:42 -0500

    fuzzy join by datettime

--- diet_log/airtable_prep.R
+++ diet_log/airtable_prep.R
@@ -49,15 +49,82 @@ extract_features <- function(x) {
 # extract_features(food.0$Protein)
 # extract_features(food.0$Nuts)
 
-food.0 %>% 
-  select(Veg, Fruit, Protein, Nuts, Extras, Snacks) %>% 
+food.1 <- 
+  food.0 %>% 
+  select(Veg, Fruit, Protein, Nuts, Extras, Snacks, Follow.ups) %>% 
   map_dfc(extract_features) %>% 
   bind_cols(
     food.0 %>% 
-      select(Date, Veg.Type, Follow.ups)
+      mutate(
+        dim_date = as.POSIXct(strptime(Date, "%F %I:%M%p")),
+        veg_cooked = ifelse(Veg.Type == "cooked", TRUE, FALSE)
+      ) %>% 
+      select(dim_date, veg_cooked)
   ) %>% 
-  select(Date, everything()) %>% 
+  select(dim_date, veg_cooked, everything()) %>% 
   glimpse()
 
 
+food.1 %>% 
+  pivot_longer(!dim_date) %>% 
+  ggplot(aes(x = dim_date, y = reorder(name, value), fill = value)) +
+  geom_tile()
 
+
+poop.1 <- 
+  poop.0 %>% 
+  mutate(
+    dim_date = as.POSIXct(strptime(Date, "%m/%d/%Y %I:%M%p")),    
+    across(c(Speed, Quality, Quantity, Color), ~ tolower(trimws(gsub("\\W", "", .x))))
+  ) %>% 
+  bind_cols(extract_features(poop.0$Checks)) %>% 
+  select(-Date, -Checks, -Notes) %>% 
+  select(dim_date, everything()) %>% 
+  glimpse()
+
+
+# JOIN ####
+fuzzy_keys <- 
+  expand.grid(
+    food = food.1$dim_date,
+    poop = poop.1$dim_date
+  ) %>% 
+  mutate(
+    delta = difftime(poop, food, units = "hours")
+  ) %>% 
+  filter(delta > 12 & delta < 40) %>% 
+  arrange(food, delta) %>% 
+  group_by(food) %>% 
+  slice_head(n = 1) %>% 
+  ungroup()
+
+food.1 %>% anti_join(fuzzy_keys, by = c("dim_date" = "food"))
+
+poop.1 %>% anti_join(fuzzy_keys, by = c("dim_date" = "poop"))
+
+all.1 <- 
+  food.1 %>% 
+  left_join(
+    fuzzy_keys,
+    by = c("dim_date" = "food")
+  ) %>% 
+  left_join(
+    poop.1,
+    by = c("poop" = "dim_date")
+  ) %>% 
+  filter(!is.na(poop)) %>% 
+  glimpse()
+
+summary(all.1$Wipes)
+
+fit.rf <- 
+  all.1 %>% 
+  filter(!is.na(Wipes)) %>% 
+  ranger::ranger(Wipes ~ . -dim_date -poop, data = ., importance = "impurity")
+
+fit.rf
+ranger::importance(fit.rf) %>% 
+  as_tibble(rownames = "pred") %>% 
+  ggplot(aes(x = reorder(pred, value), y = value)) +
+  geom_bar(stat = "identity") +
+  coord_flip()
